11ad8b3e13361039f22373b6aab500226303579a
Edward Phillips <edd0ddi0@gmail.com>
2020-05-18 14:37:55 +0100

    RF: converted date from hardcoded value to variable date dependent on time of deposit/withdrawal

--- lib/account.rb
+++ lib/account.rb
@@ -20,9 +20,12 @@ class Account
   end
   def store_transaction(num)
     if num > 0
-      @transactions.push("18/05/2020 || #{num}.00 ||  || #{@balance}.00")
+      @transactions.push("#{date} || #{num}.00 ||  || #{@balance}.00")
     else
-      @transactions.push("18/05/2020 ||  || #{num.abs}.00 || #{@balance}.00")
+      @transactions.push("#{date} ||  || #{num.abs}.00 || #{@balance}.00")
     end
+  end
+  def date
+    Time.now.strftime("%d/%m/%Y")
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,21 +1,25 @@
 require 'account'
 
 describe Account do
-  it ' should have an account balance' do
-    expect(subject.balance).to be_instance_of Integer
+
+  describe "basic account function" do
+    it ' should have an account balance' do
+      expect(subject.balance).to be_instance_of Integer
+    end
+    it ' should have a deposit method that increases the balance' do
+      n = 500
+      expect { subject.deposit(n) }.to change { subject.balance }.from(0).to(n)
+    end
+    it ' should have a withdraw method that reduces the balance' do
+      n = 500
+      subject.deposit(n)
+      expect { subject.withdraw(n) }.to change { subject.balance }.from(n).to(0)
+    end
+    it 'should be able to produce a statement' do
+      expect(subject.print_statement).to eq("date || credit || debit || balance")
+    end
   end
-  it ' should have a deposit method that increases the balance' do
-    n = 500
-    expect { subject.deposit(n) }.to change { subject.balance }.from(0).to(n)
-  end
-  it ' should have a withdraw method that reduces the balance' do
-    n = 500
-    subject.deposit(n)
-    expect { subject.withdraw(n) }.to change { subject.balance }.from(n).to(0)
-  end
-  it 'should be able to produce a statement' do
-    expect(subject.print_statement).to eq("date || credit || debit || balance")
-  end
+
   describe 'statement details' do
     it ' should show a previously made deposit on the statement' do
       n = 500
