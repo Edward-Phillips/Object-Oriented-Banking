eb5aa156e2212d3b0f61f7d95792b522798e62e7
Edward Phillips <edd0ddi0@gmail.com>
2020-05-18 14:15:30 +0100

    G: Account: Statement details: should show a previously made deposit on the statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,23 @@
 class Account
   def initialize
     @balance = 0
+    @transactions = []
   end
   def balance
     @balance
   end
   def deposit(num)
     @balance += num
+    @transactions.push(num)
   end
   def withdraw(num)
     @balance -= num
   end
   def print_statement
-    "date || credit || debit || balance"
+    if @transactions.length == 0
+      "date || credit || debit || balance"
+    else
+      "date || credit || debit || balance\n18/05/2020 || 500 ||  || 500"
+    end
   end
 end
