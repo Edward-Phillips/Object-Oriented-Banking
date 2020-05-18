138e602c208b9a651242bfac0e08b6b1c7ff0869
Edward Phillips <edd0ddi0@gmail.com>
2020-05-18 13:40:25 +0100

    test adjustment

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,6 @@ require "account"
 
 describe Account do
   it " should have an account balance" do
-    expect(subject.balance).to be_an_instance_of Numeric
+    expect(subject.balance).to be_instance_of Integer
   end
 end
