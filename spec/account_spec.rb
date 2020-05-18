ffbe875bd8d803a8e0f74985507d522c56726ed7
Edward Phillips <edd0ddi0@gmail.com>
2020-05-18 16:15:46 +0100

    R: edge cases: should not allow a withdrawal without any deposit value passed

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -61,4 +61,7 @@ describe Account do
     n = 5.005
     expect { subject.withdraw(n) }.to raise_error("Error: Invalid request")
   end
+  it ' should not allow a withdrawal without any deposit value passed' do
+    expect { subject.withdraw() }.to raise_error("Error: Invalid request")
+  end
 end
