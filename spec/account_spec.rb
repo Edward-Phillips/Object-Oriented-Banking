f31643e47089ebd5647852aeca7d6856b6d56074
Edward Phillips <edd0ddi0@gmail.com>
2020-05-18 13:47:29 +0100

    R: depositing increases the balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,7 @@ describe Account do
   it " should have an account balance" do
     expect(subject.balance).to be_instance_of Integer
   end
+  it " should have a deposit method that increases the balance" do
+    expect{ subject.deposit(500) }.to change { subject.balance }.from(0).to(500)
+  end
 end
