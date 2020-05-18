29d7b1e583cb2c59f13f34802dadd15a943a51c6
Edward Phillips <edd0ddi0@gmail.com>
2020-05-18 14:22:10 +0100

    RF: extracted transaction storing to separate method

--- lib/account.rb
+++ lib/account.rb
@@ -9,12 +9,20 @@ class Account
   end
   def deposit(num)
     @balance += num
-    @transactions.push("18/05/2020 || #{num} ||  || #{@balance}")
+    store_transaction(num)
   end
   def withdraw(num)
     @balance -= num
+    store_transaction(-num)
   end
   def print_statement
     @transactions.join("\n")
+  end
+  def store_transaction(num)
+    if num > 0
+      @transactions.push("18/05/2020 || #{num} ||  || #{@balance}")
+    else
+      @transactions.push("18/05/2020 ||  || #{num} || #{@balance}")
+    end
   end
 end
