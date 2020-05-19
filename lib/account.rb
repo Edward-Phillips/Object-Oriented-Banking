f753b482b4c409edc9ff210622d631df8f9fb023
Edward Phillips <edd0ddi0@gmail.com>
2020-05-19 11:26:52 +0100

    changes per rubocop style guidelines

--- lib/account.rb
+++ lib/account.rb
@@ -1,28 +1,25 @@
-class Account
-  COLUMN_TITLES = "date || credit || debit || balance"
+# frozen_string_literal: true
 
+# this is the Account class, it tracks the balance
+class Account
+  COLUMN_TITLES = 'date || credit || debit || balance'
+  attr_reader :balance
   def initialize
     @balance = 0
     @transactions = [COLUMN_TITLES]
   end
 
-  def balance
-    @balance
-  end
-
-  def deposit(num= '')
-    valid_input(num)
+  def deposit(num = '')
+    validate_input(num)
 
     @balance += num
     store_transaction(num)
   end
 
-  def withdraw(num='')
-    valid_input(num)
-    if num > @balance
-      raise "Error: Insufficent funds"
-    end
-    
+  def withdraw(num = '')
+    validate_input(num)
+    raise 'Error: Insufficent funds' if num > @balance
+
     @balance -= num
     store_transaction(-num)
   end
@@ -33,32 +30,26 @@ class Account
 
   private
 
-  def valid_input(num)
-    if !num.is_a? Numeric
-      raise "Error: Invalid request"
-    end
-    if num < 0
-      raise "Error: Cannot make negative transaction"
-    end
-    if decimals(num) > 0
-      raise "Error: Invalid request"
-    end
+  def validate_input(num)
+    raise 'Error: Invalid request' unless num.is_a? Numeric
+    raise 'Error: Cannot make negative transaction' if num.negative?
+    raise 'Error: Invalid request' if decimals(num) > 2
   end
 
   def decimals(num)
     decimals = 0
     number = num
-    while (number != number.to_i)
+    while number != number.to_i
       decimals += 1
       number *= 10
     end
-    return decimals
+    decimals
   end
 
   def store_transaction(num)
     amount = format_number(num.abs)
     nice_balance = format_number(@balance)
-    if num > 0
+    if num.positive?
       @transactions.push("#{date} || #{amount} ||  || #{nice_balance}")
     else
       @transactions.push("#{date} ||  || #{amount} || #{nice_balance}")
@@ -66,10 +57,10 @@ class Account
   end
 
   def date
-    Time.now.strftime("%d/%m/%Y")
+    Time.now.strftime('%d/%m/%Y')
   end
 
   def format_number(num)
-    '%.2f' % num
+    format('%.2f', num)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,7 @@
 require 'account'
 
 describe Account do
-
-  describe "basic account function" do
+  describe 'basic account function' do
     it ' should have an account balance' do
       expect(subject.balance).to be_kind_of Numeric
     end
@@ -16,7 +15,8 @@ describe Account do
       expect { subject.withdraw(n) }.to change { subject.balance }.from(n).to(0)
     end
     it 'should be able to produce a statement' do
-      expect(subject.print_statement).to eq("date || credit || debit || balance")
+      expected_output = 'date || credit || debit || balance'
+      expect(subject.print_statement).to eq(expected_output)
     end
   end
 
@@ -24,48 +24,55 @@ describe Account do
     it ' should show a previously made deposit on the statement' do
       n = 500
       subject.deposit(n)
-      expect(subject.print_statement).to eq("date || credit || debit || balance\n18/05/2020 || 500.00 ||  || 500.00")
+      expected_output = "date || credit || debit || balance\n18/05/2020 || 500.00 ||  || 500.00"
+      expect(subject.print_statement).to eq(expected_output)
     end
     it ' should show a previously made withdrawal on the statement' do
       n = 500
       subject.deposit(n)
       subject.withdraw(n)
-      expect(subject.print_statement).to eq("date || credit || debit || balance\n18/05/2020 || 500.00 ||  || 500.00\n18/05/2020 ||  || 500.00 || 0.00")
+      expected_output = "date || credit || debit || balance\n18/05/2020 || 500.00 ||  || 500.00\n18/05/2020 ||  || 500.00 || 0.00"
+      expect(subject.print_statement).to eq(expected_output)
     end
   end
   describe ' edge cases' do
     it ' should not allow non-numeric deposits' do
-      expect { subject.deposit("LOADSAMONEY") }.to raise_error("Error: Invalid request")
+      error_msg = 'Error: Invalid request'
+      expect { subject.deposit('LOADSAMONEY') }.to raise_error(error_msg)
     end
     it ' should not allow negative deposits' do
       n = -500
-      expect { subject.deposit(n) }.to raise_error("Error: Cannot make negative transaction")
+      error_msg = 'Error: Cannot make negative transaction'
+      expect { subject.deposit(n) }.to raise_error(error_msg)
     end
     it ' should not allow deposits with more than 2 decimal places' do
       n = 5.005
-      expect { subject.deposit(n) }.to raise_error("Error: Invalid request")
+      expect { subject.deposit(n) }.to raise_error('Error: Invalid request')
     end
     it ' should not allow a deposit without any deposit value passed' do
-      expect { subject.deposit() }.to raise_error("Error: Invalid request")
+      expect { subject.deposit }.to raise_error('Error: Invalid request')
     end
   end
 
   it ' should not allow non-numeric withdrawals' do
-    expect { subject.withdraw("LOADSAMONEY") }.to raise_error("Error: Invalid request")
+    bad_input = 'LOADSAMONEY'
+    error_msg = 'Error: Invalid request'
+    expect { subject.withdraw(bad_input) }.to raise_error(error_msg)
   end
   it ' should not allow negative withdrawals' do
     n = -500
-    expect { subject.withdraw(n) }.to raise_error("Error: Cannot make negative transaction")
+    error_msg = 'Error: Cannot make negative transaction'
+    expect { subject.withdraw(n) }.to raise_error(error_msg)
   end
   it ' should not allow withdrawals with more than 2 decimal places' do
     n = 5.005
-    expect { subject.withdraw(n) }.to raise_error("Error: Invalid request")
+    expect { subject.withdraw(n) }.to raise_error('Error: Invalid request')
   end
   it ' should not allow a withdrawal without any deposit value passed' do
-    expect { subject.withdraw() }.to raise_error("Error: Invalid request")
+    expect { subject.withdraw }.to raise_error('Error: Invalid request')
   end
   it ' should not allow a withdrawal greater than the account balance' do
-    n= 100
-    expect { subject.withdraw(n) }.to raise_error("Error: Insufficent funds")
+    n = 100
+    expect { subject.withdraw(n) }.to raise_error('Error: Insufficent funds')
   end
 end
