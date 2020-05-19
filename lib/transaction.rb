d3b4e7dabb8c66051ad248a6381a4020282e0bec
Edward Phillips <edd0ddi0@gmail.com>
2020-05-19 15:04:09 +0100

    G:Tx: knows the deposit value when a deposit is made

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,10 +2,11 @@
 
 # this is the Transaction class, it stores the details of each transaction
 class Transaction
-  attr_reader :type, :date, :balance
+  attr_reader :type, :date, :balance, :value
   def initialize(type:, date:, balance:, value:)
     @type = type
     @date = date
     @balance = balance
+    @value = value
   end
 end
