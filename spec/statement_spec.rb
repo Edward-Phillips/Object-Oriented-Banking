262525d572452fb01d3ec0406b20acb49bf3d744
Edward Phillips <edd0ddi0@gmail.com>
2020-05-20 10:47:23 +0100

    R: Statement: prints correct headers in statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,4 +4,7 @@ describe Statement do
   it 'is initialized with an empty array to hold transactions' do
     expect(subject.transactions.class).to eq(Array)
   end
+  it 'prints correct headers in statement' do
+    expect(subject.print_statement).to eq('date || credit || debit || balance')
+  end
 end
