fe2aeb9b6000778f405988df20d7e515b4a9aa85
Edward Phillips <edd0ddi0@gmail.com>
2020-05-20 12:48:19 +0100

    RF:rubocop approved test file

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,41 +1,48 @@
 # frozen_string_literal: true
+
 require 'transaction'
 
-describe Transaction do
-  describe 'deposits' do
-    it ' knows that it is a credit' do
-      tx = Transaction.new(type: "credit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.type).to eq("credit")
-    end
-    it ' knows the date a deposit was made' do
-      tx = Transaction.new(type: "credit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.date).to eq("18/05/2020")
-    end
-    it ' knows the balance when a deposit is made' do
-      tx = Transaction.new(type: "credit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.balance).to eq(100)
-    end
-    it ' knows the deposit value when a deposit is made' do
-      tx = Transaction.new(type: "credit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.value).to eq(100)
-    end
+describe 'deposits' do
+  before(:all) do
+    @tx = Transaction.new(
+      type: 'credit',
+      date: '18/05/2020',
+      balance: 100,
+      value: 100
+    )
   end
-  describe 'withdrawals' do
-    it ' knows that it is a debit' do
-      tx = Transaction.new(type: "debit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.type).to eq("debit")
-    end
-    it ' knows the date a withdrawal was made' do
-      tx = Transaction.new(type: "debit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.date).to eq("18/05/2020")
-    end
-    it ' knows the balance when a withdrawal is made' do
-      tx = Transaction.new(type: "debit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.balance).to eq(100)
-    end
-    it ' knows the deposit value when a withdrawal is made' do
-      tx = Transaction.new(type: "debit", date:"18/05/2020", balance: 100, value: 100)
-      expect(tx.value).to eq(100)
-    end
+  it ' knows that it is a credit' do
+    expect(@tx.type).to eq('credit')
+  end
+  it ' knows the date a deposit was made' do
+    expect(@tx.date).to eq('18/05/2020')
+  end
+  it ' knows the balance when a deposit is made' do
+    expect(@tx.balance).to eq(100)
+  end
+  it ' knows the deposit value when a deposit is made' do
+    expect(@tx.value).to eq(100)
+  end
+end
+describe 'withdrawals' do
+  before(:all) do
+    @tx = Transaction.new(
+      type: 'debit',
+      date: '18/05/2020',
+      balance: 100,
+      value: 100
+    )
+  end
+  it ' knows that it is a debit' do
+    expect(@tx.type).to eq('debit')
+  end
+  it ' knows the date a withdrawal was made' do
+    expect(@tx.date).to eq('18/05/2020')
+  end
+  it ' knows the balance when a withdrawal is made' do
+    expect(@tx.balance).to eq(100)
+  end
+  it ' knows the withdrawal value when a withdrawal is made' do
+    expect(@tx.value).to eq(100)
   end
 end
