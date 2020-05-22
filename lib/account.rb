0b46186d42fb39629d82303aa16b07356f9f2085
Edward Phillips <edd0ddi0@gmail.com>
2020-05-22 12:34:28 +0100

    RF:  Make Account Class readable, simplify methods

--- lib/account.rb
+++ lib/account.rb
@@ -3,63 +3,59 @@
 require_relative 'transaction'
 require_relative 'statement'
 
-# this is the Account class, it tracks the balance
 class Account
   attr_reader :balance
   def initialize(
     transaction_class: Transaction,
-    statement_class: Statement,
-    time: Time
+    statement_class: Statement
   )
     @balance = 0
     @statement = statement_class.new
     @transaction_class = transaction_class
-    @time = time
+    @transaction_history = []
   end
 
-  def deposit(num = '')
-    validate_input(num)
+  def deposit(number = nil)
+    validate_input(number)
 
-    @balance += num
-    store_transaction(num, 'credit')
+    @balance += number
+    store_transaction(number, 'credit')
+    nil
   end
 
-  def withdraw(num = '')
-    validate_input(num)
-    raise 'Error: Insufficent funds' if num > @balance
+  def withdraw(number = nil)
+    validate_input(number)
+    raise 'Error: Insufficent funds' if number > @balance
 
-    @balance -= num
-    store_transaction(-num, 'debit')
+    @balance -= number
+    store_transaction(number, 'debit')
+    nil
   end
 
   def print_statement
-    @statement.print_statement
+    print @statement.print_statement(@transaction_history)
   end
 
   private
 
-  def validate_input(num)
-    raise 'Error: Invalid request' unless num.is_a? Numeric
-    raise 'Error: Cannot make negative transaction' if num.negative?
-    raise 'Error: Invalid request' if decimals(num) > 2
+  def validate_input(number)
+    raise 'Error: Invalid request' unless number.is_a? Numeric
+    raise 'Error: Cannot make transaction of 0.00 or lower' if number <= 0
+    raise 'Error: Invalid request' unless decimals(number)
   end
 
   def decimals(number)
-    decimals = 0
-    while number != number.to_i
-      decimals += 1
-      number *= 10
-    end
-    decimals
+    pennies = number * 100
+    pennies == pennies.to_i
   end
 
-  def store_transaction(num, type)
+  def store_transaction(number, type)
     transaction = @transaction_class.new(
       type: type,
-      date: @time.now,
+      date: Time.now,
       balance: @balance,
-      value: num.abs
+      value: number.abs
     )
-    @statement.transactions.push(transaction)
+    @transaction_history.push(transaction)
   end
 end
